b3d2ed78ff8c4c999b50da9f3fb658806e640dda
Piethein Strengholt <piethein@xbmcfreak.nl>
2011-03-27 12:17:54 +0200

    fix indendation

--- build.sh
+++ build.sh
@@ -98,11 +98,11 @@ if ! which lh > /dev/null ; then
 		if [ ! -f live-build.tar ]; then
 			git clone git://live.debian.net/git/live-build.git 
 			pushd live-build > /dev/null
-                        git checkout -b 2.0_a22-1 debian/2.0_a22-1
-                        if [ "$?" -ne "0" ]; then
+			git checkout -b 2.0_a22-1 debian/2.0_a22-1
+			if [ "$?" -ne "0" ]; then
 				exit 1
 			fi
-                        popd > /dev/null
+			popd > /dev/null
 
 
 			# Saved, to avoid cloning for multiple builds
